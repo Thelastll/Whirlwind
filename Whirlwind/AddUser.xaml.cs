3526629b285c689407c0e2f0a4864440999e00f7
The Last Left <prostodima54769@gmail.com>
2026-04-25 17:52:03 +0300

    v0.0.1

---
+++ Whirlwind/AddUser.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Whirlwind
+{
+    /// <summary>
+    /// Логика взаимодействия для AddUser.xaml
+    /// </summary>
+    public partial class AddUser : Window
+    {
+        public AddUser()
+        {
+            InitializeComponent();
+        }
+
+        public string IpAddresssee { get; private set; } = null;
+        public string NameAddresssee { get; private set; } = null;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (add_ip_address.Text.Trim() == "") return;
+            if (add_name.Text.Trim() == "") add_name.Text = "_";
+
+            IpAddresssee = add_ip_address.Text.Trim();
+            NameAddresssee = add_name.Text.Trim();
+
+            Close();
+        }
+    }
+}
---
+++ Whirlwind/EnterWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Whirlwind
+{
+    /// <summary>
+    /// Логика взаимодействия для EnterWindow.xaml
+    /// </summary>
+    public partial class EnterWindow : Window
+    {
+        public EnterWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string IpSender { get; private set; } = null;
+        private bool ChangeButtonClicked = false;
+
+        private void enter_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                enter_button.IsEnabled = false;
+
+                Native.test_ip_port_sender(ip.Text == "" ? "_" : ip.Text, Properties.Settings.Default.port_sender,
+                (ptr, len) =>
+                {
+                    byte[] buffer = new byte[len];
+                    Marshal.Copy(ptr, buffer, 0, len);
+                    string result = Encoding.UTF8.GetString(buffer);
+
+                    Dispatcher.Invoke(() =>
+                    {
+                        IpSender = result;
+                        enter_button.IsEnabled = true;
+                        ChangeButtonClicked = true;
+                    });
+
+                    Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        Close();
+                    }));
+                },
+                (ptr, len) =>
+                {
+                    byte[] buffer = new byte[len];
+                    Marshal.Copy(ptr, buffer, 0, len);
+                    string result = Encoding.UTF8.GetString(buffer);
+
+                    Dispatcher.Invoke(() =>
+                    {
+                        MessageBox.Show(result);
+                        enter_button.IsEnabled = true;
+                    });
+                });
+            }
+            catch {
+                enter_button.IsEnabled = true;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            IpSender = Properties.Settings.Default.ip_sender == null || ChangeButtonClicked ? IpSender : Properties.Settings.Default.ip_sender;
+        }
+    }
+}
